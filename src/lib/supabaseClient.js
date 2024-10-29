037f7c4edd249d8e50f20768e1cbc0c83860c130
Dillon Lara <dillonlara115@gmail.com>
2024-10-29 17:43:42 -0600

    implementing shadcn and pulling taliwind css into application.

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://ejusgyjfiyvcgzdmonew.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdXNneWpmaXl2Y2d6ZG1vbmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyMzEyNDQsImV4cCI6MjA0NTgwNzI0NH0.1yqmXs6M4c03HCJ6qINn1TckKq1mbHQCD0rHC6lgWNQ')
