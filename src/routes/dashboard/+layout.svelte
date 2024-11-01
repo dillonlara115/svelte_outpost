b17ec8c137996a30bdaab45cb401b3958d6e4212
Dillon Lara <dillonlara115@gmail.com>
2024-11-01 14:16:27 -0600

    adding table and select components from shadcn and business types dropdown for search

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -33,8 +33,7 @@
 	<div class="bg-muted/40 hidden border-r md:block">
 	  <div class="flex h-full max-h-screen flex-col gap-2">
 		<div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-		  <a href="/" class="flex items-center gap-2 font-semibold">
-			<Package2 class="h-6 w-6" />
+		  <a href="/dashboard" class="flex items-center gap-2 font-semibold">
 			<span class="">Outpost Leads</span>
 		  </a>
 		</div>
@@ -49,18 +48,18 @@
 			  Dashboard
 			</a>
 			<a
-			  href="##"
-			  class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
-			>
-			  <Building2 class="h-4 w-4" />
-			  Saved Searches
-			</a>
-			<a
 			  href="/dashboard/search"
 			  class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
 			>
 			  <Search class="h-4 w-4" />
 			  Find Leads
+			</a>
+			<a
+			  href="##"
+			  class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
+			>
+			  <Building2 class="h-4 w-4" />
+			  Saved Searches
 			</a>
 		  </nav>
 		</div>
@@ -107,18 +106,18 @@
 				Dashboard
 			  </a>
 			  <a
-				href="##"
-				class="bg-muted text-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
-			  >
-				<Building2 class="h-5 w-5" />
-				Saved Searches
-			  </a>
-			  <a
 				href="/dashboard/search"
 				class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
 			  >
 				<Search class="h-5 w-5" />
 				Find Leads
+			  </a>
+			  <a
+				href="##"
+				class="bg-muted text-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+			  >
+				<Building2 class="h-5 w-5" />
+				Saved Searches
 			  </a>
 			</nav>
 			<div class="mt-auto">
